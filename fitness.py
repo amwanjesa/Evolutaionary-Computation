b758c51c7f7abd64a57d595f5f5cefae2a73c0ce
amwanjesa <a.mwanjesa@gmail.com>
2020-02-13 17:06:56 +0100

    Initial commit

---
+++ fitness.py
@@ -0,0 +1,7 @@
+class Fitness:
+
+    def __init__(self):
+        pass
+
+    def count_ones(self, solution):
+        return solution.count('1')
---
+++ genetic_algorithm.py
@@ -0,0 +1,12 @@
+class GA:
+    def __init__(self, population):
+        self.population = population
+
+    def crossover(self):
+        pass
+
+    def mutate(self):
+        pass 
+
+    def family_competition(self, k=2):
+        pass
