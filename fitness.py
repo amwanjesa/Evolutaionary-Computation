6c732162ccd3020636a48c5053a03791e9006cae
amwanjesa <a.mwanjesa@gmail.com>
2020-02-13 18:05:54 +0100

    Added trap function

--- fitness.py
+++ fitness.py
@@ -5,3 +5,13 @@ class Fitness:
 
     def count_ones(self, solution):
         return solution.count('1')
+    
+    def trap_function(self, solution, k, d):
+        fitness = 0
+        scalar = (k - d) / (k - 1)
+        for subsolution in zip(*(iter(solution),) * k):
+            if self.count_ones(subsolution) == k:
+                fitness += k
+            elif self.count_ones(subsolution) < k:
+                fitness += k - d - scalar * self.count_ones(solution)
+        return fitness
