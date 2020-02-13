9fbd7bd64151da6cc2c3ba3f36f0210ae8ef6c7a
amwanjesa <a.mwanjesa@gmail.com>
2020-02-13 17:23:30 +0100

    Minor change

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,12 +1,21 @@
+import random
+
+
 class GA:
-    def __init__(self, population):
-        self.population = population
+    def __init__(self, population_size):
+        self.population = self.generate_population(length=)
 
     def crossover(self):
         pass
 
     def mutate(self):
-        pass 
+        pass
 
     def family_competition(self, k=2):
         pass
+
+    def generate_population(self, length=100, size=10):
+        def get_binary_string(length):
+            return ''.join((random.choice('01') for i in range(length)))
+
+        return [get_binary_string(length) for i in range(size)]
