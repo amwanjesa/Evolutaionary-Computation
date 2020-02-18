4c0694d575eabbecac193806325f8a8aafa0d73c
amwanjesa <a.mwanjesa@gmail.com>
2020-02-18 19:26:31 +0100

    Experiments can be run

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,11 +1,11 @@
-import random, heapq
+import random
 from fitness import Fitness
-random.seed(23)
+random.seed(42)
 
 class GA:
     def __init__(self, population_size):
         self.population = self.generate_population(length=100, size=population_size)
-        self.new_population = self.create_new_population()
+        self.generation = 0
 
     def create_new_population(self):
         # shuffle the population
@@ -19,22 +19,23 @@ class GA:
         # make them compete
             selection = self.family_competition(k=2)
         # return the new population
-            new_population.append(selection)
-        return new_population
+            new_population += selection
+        self.population = new_population
+        self.generation += 1
 
     def crossover(self, length=100, crossover_type='2X'):
         # choose crossover type (two point or uniform)
         # receive two parents and compute the children
-        child = []
+        children = []
         if crossover_type == '2X':
             # two point crossover (2X)
             cross_point = [random.randint(0, length), random.randint(0, length)]
             while cross_point[0] == cross_point[1]:
-                cross_point[1] == random.randint(0, length)
+                cross_point[1] = random.randint(0, length)
             cross_point.sort()
-            child.append(self.parent[0][0:cross_point[0]] + self.parent[1][cross_point[0]:cross_point[1]] + self.parent[0][cross_point[1]:length])
-            child.append(self.parent[1][0:cross_point[0]] + self.parent[0][cross_point[0]:cross_point[1]] + self.parent[1][cross_point[1]:length])
-            return child
+            children.append(self.parent[0][0:cross_point[0]] + self.parent[1][cross_point[0]:cross_point[1]] + self.parent[0][cross_point[1]:length])
+            children.append(self.parent[1][0:cross_point[0]] + self.parent[0][cross_point[0]:cross_point[1]] + self.parent[1][cross_point[1]:length])
+            return children
         else:
             # uniform crossover (UX)
             child_1 = ''
@@ -47,19 +48,33 @@ class GA:
                 else:
                     child_1 += self.parent[0][i]
                     child_2 += self.parent[1][i]
-            child = [child_1, child_2]
-            return child
+            children = [child_1, child_2]
+            return children
 
     def family_competition(self, k=2):
         family = self.parent + self.children
-
-        family_fitness = {solution:Fitness.count_ones(solution) for solution in family}          
-        sorted_family = sorted(family_fitness.items(), key = lambda x: x[1])
         
-        return [sorted_family[len(sorted_family)-1][0], sorted_family[len(sorted_family)-2][0]]
+        family_fitness = [(solution, Fitness.count_ones(solution)) for solution in family]          
+        sorted_family = sorted(family_fitness, key = lambda x: x[1], reverse=True)
+        kth_best, kth1_best = sorted_family[k - 1], sorted_family[k]
 
+        if kth_best[1] == kth_best[1]:
+            if kth1_best[0] in self.parent and not kth_best in self.parent:
+                sorted_family[k - 1] = kth1_best
+                sorted_family[k] = kth_best
+        
+        return [solution[0] for solution in sorted_family[:k]]
+    
+    def global_optimum_found(self, length=100):
+        global_optimum = '1' * length
+        return global_optimum in self.population
+        
     def generate_population(self, length=100, size=10):
         def get_binary_string(length):
             return ''.join((random.choice('01') for i in range(length)))
 
         return [get_binary_string(length) for i in range(size)]
+
+    def population_stats(self):
+        return [Fitness.count_ones(solution) for solution in self.population]
+            
--- testing.py
+++ testing.py
@@ -1,5 +1,12 @@
 from genetic_algorithm import GA
 
 if __name__ == "__main__":
-    x = GA(10)
+    ga = GA(10)
+    stats = {ga.generation: ga.population_stats()}
+
+
+    while not ga.global_optimum_found():
+        ga.create_new_population()
+        stats[ga.generation] = ga.population_stats()
+    print(f'Found global minimum!')
     
