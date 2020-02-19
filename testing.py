9e4973ec0eb5f941f450c4de46953bb93ded7814
Alsina <albayalsina34@gmail.com>
2020-02-19 10:50:29 +0100

    Update testing file

--- testing.py
+++ testing.py
@@ -1,12 +1,20 @@
 from genetic_algorithm import GA
 
 if __name__ == "__main__":
-    ga = GA(10)
+    ga = GA(70)
     stats = {ga.generation: ga.population_stats()}
 
-
-    while not ga.global_optimum_found():
+    i = 0
+    iterations = 1000
+    while not ga.global_optimum_found() and i < iterations:
+        i += 1
         ga.create_new_population()
         stats[ga.generation] = ga.population_stats()
-    print(f'Found global minimum!')
+        #print(i)
+
+    if i == iterations:
+        print(f'No global minimum :(')
+    else:
+        print(f'Found global minimum!')
+    print(stats)
     
