1b599d8cef12a429919223891d960aa0b7cd90cf
amwanjesa <a.mwanjesa@gmail.com>
2020-02-20 21:09:21 +0100

    Moved to function, tired of linter warnings

--- testing.py
+++ testing.py
@@ -4,7 +4,10 @@ import time
 from genetic_algorithm import GA
 
 if __name__ == "__main__":
+    run_experiment()
 
+
+def run_experiment():
     generation_fitness = pd.DataFrame()
     successful_generations = pd.DataFrame(columns=['n_generations', 'success'])
 
@@ -18,13 +21,13 @@ if __name__ == "__main__":
             ga.create_new_population()
 
         successful_generations = successful_generations.append({'n_generations': ga.generation + 1,
-                           'success': ga.global_optimum_found()}, ignore_index=True)
+                                                                'success': ga.global_optimum_found()}, ignore_index=True)
         generation_fitness[f'Run {i + 1}'] = ga.population_stats()
 
     toc = time.perf_counter()
     print(f" in {toc - tic:0.4f} seconds")
 
-generation_fitness.to_csv(
-    f'generation_fitness_n_{population_size}_{fitness_function}.csv')
-successful_generations.to_csv(
-    f'successful_generations_n_{population_size}_{fitness_function}.csv')
+    generation_fitness.to_csv(
+        f'generation_fitness_n_{population_size}_{fitness_function}.csv')
+    successful_generations.to_csv(
+        f'successful_generations_n_{population_size}_{fitness_function}.csv')
