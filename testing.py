56de7334df7f2571d996a35e825242b41ff0b6e1
Alsina <albayalsina34@gmail.com>
2020-02-20 22:32:45 +0100

    Create stats table

--- testing.py
+++ testing.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import time
+import numpy as np
 
 from genetic_algorithm import GA
-
-if __name__ == "__main__":
-    run_experiment()
-
 
 def run_experiment():
     generation_fitness = pd.DataFrame()
@@ -27,7 +24,30 @@ def run_experiment():
     toc = time.perf_counter()
     print(f" in {toc - tic:0.4f} seconds")
 
-    generation_fitness.to_csv(
-        f'generation_fitness_n_{population_size}_{fitness_function}.csv')
-    successful_generations.to_csv(
-        f'successful_generations_n_{population_size}_{fitness_function}.csv')
+    #generation_fitness.to_csv(
+    #    f'generation_fitness_n_{population_size}_{fitness_function}.csv')
+    #successful_generations.to_csv(
+    #    f'successful_generations_n_{population_size}_{fitness_function}.csv')
+
+    # table stats
+    average_generations = successful_generations.mean(axis = 0, skipna = True)[0]
+    sd_generations = successful_generations.std(axis = 0, skipna = True)[0]
+    row_average_fitness = generation_fitness.mean(axis = 1, skipna = True)
+    row_sd_fitness = generation_fitness.std(axis = 1, skipna = True)
+    average_fitness = row_average_fitness.mean(axis = 0, skipna = True)
+    sd_fitness = row_sd_fitness.std(axis = 0, skipna = True)
+    cpu_time = toc-tic
+
+    table_data = pd.DataFrame({'Population size': [population_size], 
+        'Average generations': [average_generations],
+        'SD generations': [sd_generations], 
+        'Average fitness': [average_fitness], 'SD fitness': [sd_fitness],
+        'CPU time': [cpu_time]})
+
+    print(table_data)
+
+    table_data.to_csv(
+        f'table_data_{population_size}_{fitness_function}.csv')
+
+if __name__ == "__main__":
+    run_experiment()
