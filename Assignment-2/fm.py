1e613ed24fb3c0ec1856cfe158d06c7628d67428
amwanjesa <a.mwanjesa@gmail.com>
2020-03-16 19:00:14 +0100

    Read graph, random initial partitions

--- Assignment-2/fm.py
+++ Assignment-2/fm.py
@@ -1,47 +1,35 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from random import shuffle
 
-def convert_to_dataframe(filename):
-    data = pd.DataFrame(columns = ['ID', 'coordinates', 'number_connections', 'ID_connections'])
+
+def read_graph_data(filename):
+
+    graph_data = {}
     with open(filename) as f:
-        graph = f.read()
-    
-    graph = graph.split('\n')
+        for line in f:
+            l = line.split()
+            if len(l) > 0:
+                graph_data[l[0]] = {'coordinates': l[1],
+                                    'degree': l[2], 'ID_connections': l[3:]}
+    return graph_data
 
-    for line in tqdm(graph):
-        l = line.split()
-        if len(l) > 0:
-            data = data.append({'ID': l[0], 'coordinates': l[1], 
-                'number_connections': l[2], 'ID_connections': l[3:]}, ignore_index=True)
-    return data
 
-def MLS(graph):
-    cell_distribution = {}
+def random_initial_partitioning(graph_data):
 
-    #random division in two subsets
-    graph_array = np.random.choice(2, graph.shape[0])
+    nodes = list(graph_data.keys())
+    shuffle(nodes)
 
-    for index, row in tqdm(graph.iterrows()):
-        subset_A = 0
-        for num in row.ID_connections:
-            if graph_array[int(num)-1] == 1:
-                subset_A += 1
+    partition_a = { node: graph_data[node] for node in nodes[:len(nodes) // 2]}
+    partition_b = { node: graph_data[node] for node in nodes[len(nodes) // 2:]}
 
-        if row.ID in cell_distribution:
-            cell_distribution[row.ID].append([graph_array[int(row.ID)-1], subset_A, int(row.number_connections)-subset_A])
-        else:
-            cell_distribution[row.ID] = [graph_array[int(row.ID)-1], subset_A, int(row.number_connections)-subset_A]
-    
-    return cell_distribution
+    return partition_a, partition_b
 
 
 if __name__ == '__main__':
 
     np.random.seed(352)
 
-    graph = convert_to_dataframe('Graph500.txt')
-    cell_distribution = MLS(graph)
-
-    
-
+    graph = read_graph_data('Graph500.txt')
+    partitions = random_initial_partitioning(graph)
