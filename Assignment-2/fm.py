33dce93e4cdfaaf6f92d71463f0706383d1635a2
Asun <asun.alsina.lopez@gmail.com>
2020-03-17 22:06:15 +0100

    create network

--- Assignment-2/fm.py
+++ Assignment-2/fm.py
@@ -1,19 +1,41 @@
 from graph import *
-
+import pandas as pd
+import pprint
 
 def read_graph_data(filename):
-
+    connections = []
+    net = []
+    counter = 0
     graph = Graph()
     with open(filename) as f:
         for line in f:
             l = line.split()
             if len(l) > 0:
+                connections.append(l[3:])
                 graph.add_node(l[0], l[2])
                 for node in l[3:]:
                     graph.add_edge(l[0], node)
-    return graph
+                
+        for i in connections:
+            counter += 1 
+            for j in i:
+                #intersection of i and connections[j]
+                inters = list(set(i).intersection(set(connections[int(j)-1])))
+                if len(inters) > 0: 
+                    inters.extend([str(counter), j])
+                    inters.sort()
+                else:
+                    inters.extend([str(counter), j])
+                    inters.sort()
+                if inters not in net:
+                    net.append(inters) 
+
+    return graph, net
 
 
 if __name__ == '__main__':
-    graph = read_graph_data('Graph500.txt')
+    graph, net = read_graph_data('Graph500.txt')
+    pp = pprint.PrettyPrinter()
+    pp.pprint(net)
+    print(len(net))
     graph.init_partition()
