6f6c1981af6aed19ac16fc02dd0bbc5759822faf
Alsina <albayalsina34@gmail.com>
2020-04-01 22:24:51 +0200

    Update stats

--- Assignment-2/stats.py
+++ Assignment-2/stats.py
@@ -3,37 +3,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import wilcoxon
 
-def get_plot(data, mode = 'MLS'): 
+def get_plot(data, ylabel, mode = 'MLS'): 
     if mode == 'ILS':
         plt.plot(data[0], label = 'Mutation 0.01')
         plt.plot(data[1], label = 'Mutation 0.03')
         plt.plot(data[2], label = 'Mutation 0.05')
+        plt.legend()
+        plt.xlabel('Run')
+        plt.ylabel(ylabel)
         plt.show()
         
         plt.boxplot(data)
         plt.xticks([1,2,3], ['ILS 0.01','ILS 0.03','ILS 0.05'])
+        plt.ylabel(ylabel)
         plt.show()
 
     if mode == 'MLS':
         plt.plot(data)
+        plt.ylabel(ylabel)
+        plt.xlabel('Run')
         plt.show()
 
         plt.boxplot(data)
         plt.xticks([1], [mode])
+        plt.ylabel(ylabel)
         plt.show()    
 
     if mode == 'GLS':
-        #plt.plot(data[0], label = 'Population 20')
-        plt.plot(data, label = 'Population 50')
+        plt.plot(data)
+        plt.ylabel(ylabel)
+        plt.xlabel('Run')
         plt.show()
 
         plt.boxplot(data)
-        #plt.xticks([1, 2], ['GLS20', 'GLS50'])
+        plt.xticks([1], [mode])
+        plt.ylabel(ylabel)
         plt.show()
 
     if mode == 'ALL':
         plt.boxplot(data)
-        plt.xticks([1,2,3,4,5], ['MLS','ILS 0.01','ILS 0.03','ILS 0.05','GLS'])#'GLS20','GLS50'])
+        plt.ylabel(ylabel)
+        plt.xticks([1,2,3,4,5], ['MLS','ILS 0.01','ILS 0.03','ILS 0.05','GLS'])
         plt.show()
 
 def get_gls_data(data):
@@ -69,6 +79,9 @@ if __name__ == '__main__':
     mls_timeout_1 = pd.read_csv('data/mls/mls_with_fm_time_limit_60.csv')
     ils_timeout_1 = pd.read_csv('data/ils/ils_with_fm_0.05_limit_60.csv')
     gls_timeout_1 = pd.read_csv('data/gls/gls_with_fm_limit_60.csv')
+    mls_timeout_20 = pd.read_csv('data/mls/mls_with_fm_time_limit_1200.csv')
+    ils_timeout_20 = pd.read_csv('data/ils/ils_with_fm_0.05_limit_1200.csv')
+    gls_timeout_20 = pd.read_csv('data/gls/gls_with_fm_limit_1200.csv')
 
 
     #### CUTSTATE ####
@@ -83,10 +96,10 @@ if __name__ == '__main__':
     average_ils_005 = np.mean(data_005)
     average_gls_data = np.mean(gls_data_cutstate)
 
-    get_plot(mls_data['cutstate'], mode = 'MLS')
-    get_plot([data_001, data_003, data_005], mode = 'ILS')
-    get_plot(gls_data_cutstate, mode = 'GLS')
-    get_plot([mls_data['cutstate'], data_001, data_003, data_005, gls_data_cutstate], mode = 'ALL')
+    get_plot(mls_data['cutstate'], ylabel= 'Cutstate', mode = 'MLS')
+    get_plot([data_001, data_003, data_005], ylabel= 'Cutstate', mode = 'ILS')
+    get_plot(gls_data_cutstate, ylabel= 'Cutstate', mode = 'GLS')
+    get_plot([mls_data['cutstate'], data_001, data_003, data_005, gls_data_cutstate], ylabel= 'Cutstate', mode = 'ALL')
 
     # Wilcoxon test
     wilcoxon_mls_gls = wilcoxon(mls_data['cutstate'], gls_data_cutstate)
@@ -118,10 +131,10 @@ if __name__ == '__main__':
     average_ils_performance005 = np.mean(performance_005)
     average_gls_performance = np.mean(gls_performance_minutes)
 
-    get_plot(mls_performance_minutes, mode = 'MLS')
-    get_plot([performance_001, performance_003, performance_005], mode = 'ILS')
-    get_plot(gls_performance_minutes, mode = 'GLS')
-    get_plot([mls_performance_minutes, performance_001, performance_003, performance_005, gls_performance_minutes], mode = 'ALL')
+    get_plot(mls_performance_minutes, ylabel= 'Time (min)', mode = 'MLS')
+    get_plot([performance_001, performance_003, performance_005], ylabel= 'Time (min)', mode = 'ILS')
+    get_plot(gls_performance_minutes, ylabel= 'Time (min)', mode = 'GLS')
+    get_plot([mls_performance_minutes, performance_001, performance_003, performance_005, gls_performance_minutes], ylabel= 'Time (min)', mode = 'ALL')
 
     # Wilcoxon test
     wilcoxon_performance_mls_gls = wilcoxon(mls_performance_minutes, gls_performance_minutes)
@@ -141,6 +154,7 @@ if __name__ == '__main__':
 
 
     #### TIMEOUT ###
+    # 1 minute
     timeout_1_001 = list(ils_timeout_1['cutstate'][:25])
     timeout_1_003 = list(ils_timeout_1['cutstate'][25:50])
     timeout_1_005 = list(ils_timeout_1['cutstate'][50:])
@@ -153,9 +167,26 @@ if __name__ == '__main__':
     average_ils_timeout_005 = np.mean(timeout_1_005)
     average_gls_timeout = np.mean(gls_timeout_cutstate)
 
-    get_plot(mls_timeout_1['cutstate'], mode = 'MLS')
-    get_plot([timeout_1_001, timeout_1_003, timeout_1_005], mode = 'ILS')
-    get_plot(gls_timeout_cutstate, mode = 'GLS')
-    get_plot([mls_timeout_1['cutstate'], timeout_1_001, timeout_1_003, timeout_1_005, gls_timeout_cutstate], mode = 'ALL')
+    get_plot(mls_timeout_1['cutstate'], ylabel= 'Cutstate', mode = 'MLS')
+    get_plot([timeout_1_001, timeout_1_003, timeout_1_005], ylabel= 'Cutstate', mode = 'ILS')
+    get_plot(gls_timeout_cutstate, ylabel= 'Cutstate', mode = 'GLS')
+    get_plot([mls_timeout_1['cutstate'], timeout_1_001, timeout_1_003, timeout_1_005, gls_timeout_cutstate], ylabel= 'Cutstate', mode = 'ALL')
 
+    # 20 minutes
+    timeout_20_001 = list(ils_timeout_20['cutstate'][:25])
+    timeout_20_003 = list(ils_timeout_20['cutstate'][25:50])
+    timeout_20_005 = list(ils_timeout_20['cutstate'][50:])
+
+    gls_timeout_cutstate_20 = get_gls_data(gls_timeout_20['Cutstate'])
+
+    average_mls_timeout_20 = np.mean(mls_timeout_20['cutstate'])
+    average_ils_timeout_001_20 = np.mean(timeout_20_001)
+    average_ils_timeout_003_20 = np.mean(timeout_20_003)
+    average_ils_timeout_005_20 = np.mean(timeout_20_005)
+    average_gls_timeout_20 = np.mean(gls_timeout_cutstate_20)
+
+    get_plot(mls_timeout_20['cutstate'], ylabel= 'Cutstate', mode = 'MLS')
+    get_plot([timeout_20_001, timeout_20_003, timeout_20_005], ylabel= 'Cutstate', mode = 'ILS')
+    get_plot(gls_timeout_cutstate_20, ylabel= 'Cutstate', mode = 'GLS')
+    get_plot([mls_timeout_20['cutstate'], timeout_20_001, timeout_20_003, timeout_20_005, gls_timeout_cutstate_20], ylabel= 'Cutstate', mode = 'ALL')
 
